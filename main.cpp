e3c13c3d17a5b93964bf956c5f169007e46023aa
Jimin7717 <168612392+Jimin7717@users.noreply.github.com>
2025-10-03 03:03:46 +0000

    A

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #define SIZE 10
 
 using namespace std;
-
+/*
 int main(int argc,char *argv[]){
   int *pa, *pb, i, temp;
 int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
@@ -33,6 +33,45 @@ for (i=0; i<SIZE-1; i++ ,pa++){
 }
 cout<<setw(3)<<*pa<<endl;
 
-
 return 0;
+*/
+
+int main() {
+    int *p, *pa, *pb, i, temp, n;
+    
+    cout << "How many numbers? " << endl;
+    cin >> n;
+    p = new int[n]; 
+    cout << "What are the numbers? " << endl;
+    for (i = 0; i < n; i++) {
+        cin >> p[i];  
+    }
+    
+    // Print original 
+    pa = p;
+    cout << "Original: ";
+    for (i = 0; i < n; i++, pa++) {
+        cout << setw(3) << *pa;
+    }
+    cout << endl;
+    
+    // Print Reverse
+    pa = p;               
+    pb = p + n - 1;   
+    for (i = 0; i < n / 2; i++) {
+        temp = *pa;
+        *pa = *pb;
+        *pb = temp;
+        pa++; pb--;
+    }
+    
+    pa = p;
+    cout << "Reversed: ";
+    for (i = 0; i < n; i++, pa++) {
+        cout << setw(3) << *pa;
+    }
+    cout << endl;
+    
+    delete[] p;
+    return 0;
 }
